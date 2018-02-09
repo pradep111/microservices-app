21bc1b23c6a5b09b8c32e18cee8f717590b22fe3
pradeep <pradep111@hotmail.com>
2018-02-09 17:13:18 +0545

    initial commit

---
+++ config.py
@@ -0,0 +1,22 @@
+
+class BaseConfig(object):
+	"""Base Configuration"""
+	DEBUG=False
+	TESTING=False
+
+
+class DevelopmentConfig(BaseConfig):
+    """Development configuration"""
+    DEBUG = True
+
+
+class TestingConfig(BaseConfig):
+    """Testing configuration"""
+    DEBUG = True
+    TESTING = True
+
+
+class ProductionConfig(BaseConfig):
+    """Production configuration"""
+    DEBUG = False	
+		
